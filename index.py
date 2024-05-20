aa7ec428bb65ababbc6703a9dac0cf05af78c2df
generic-name-2166 <kirbesson277@mail.ru>
2024-05-21 01:13:00 +0300

    fix!: idek

--- index.py
+++ index.py
@@ -22,20 +22,22 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(update.message.text)
 
 
-def main(event: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+def main() -> None:
     # Handle restarts here, somehow
     # TODO
-    application = ApplicationBuilder().token(os.environ["BOT_TOKEN"]).build()
 
+    application = ApplicationBuilder().token(os.environ["BOT_TOKEN"]).build()
+    
     start_handler = CommandHandler("start", start)
     application.add_handler(start_handler)
 
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
-    
-    application.insert_callback_data(event)
 
+
+def handler(event: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # Shouldn't this be processed in main
     return {
         "statusCode": 200,
         "body": json.dumps(
@@ -48,5 +50,4 @@ def main(event: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     }
 
 
-if __name__ == "__main__":
-    main()
+main()
