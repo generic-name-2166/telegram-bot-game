753de6cf9b99d877bca2d2ad80022b21d62d62a6
generic-name-2166 <kirbesson277@mail.ru>
2024-05-20 12:39:10 +0300

    chore: pcakge.json

--- bunbuild.ts
+++ bunbuild.ts
@@ -1,24 +1,41 @@
+import { $ } from "bun";
 import * as fs from "node:fs/promises";
 
 const OUTPUT_DIR: string = ".out";
 
-await fs.rm(OUTPUT_DIR, { recursive: true });
-
-const result = await Bun.build({
-  entrypoints: ["src/index.ts"],
-  outdir: OUTPUT_DIR,
-  target: "node",
-  format: "esm", // cjs
-  minify: true,
-  naming: "[dir]/[name].[ext]",
-});
-
-if (!result.success) {
-  console.error("Build failed");
-  for (const message of result.logs) {
-    // Bun will pretty print the message object
-    console.error(message);
-  }
-} else {
-  console.log("Success");
+async function clearOutdir(): Promise<void> {
+  await fs.rm(OUTPUT_DIR, { recursive: true });
 }
+
+async function copyAssets(): Promise<void> {
+  // Bun file I/O has undocumented ENOENT
+  return fs.copyFile("assets/out.package.json", OUTPUT_DIR + "/package.json");
+}
+
+async function main(): Promise<void> {
+  await clearOutdir();
+
+  const result = await Bun.build({
+    entrypoints: ["src/index.ts"],
+    outdir: OUTPUT_DIR,
+    target: "node",
+    format: "esm", // cjs
+    minify: true,
+    naming: "[dir]/[name].[ext]",
+  });
+  
+  if (!result.success) {
+    console.error("Build failed");
+    for (const message of result.logs) {
+      // Bun will pretty print the message object
+      console.error(message);
+    }
+    return;
+  }
+
+  await copyAssets();
+
+  console.log("Build success");
+}
+
+await main();
