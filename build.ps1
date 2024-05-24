8108ac5b4c93e31f4d76bd558c7d47bbb7044c27
generic-name-2166 <kirbesson277@mail.ru>
2024-05-24 23:44:57 +0300

    build: theoretically finished script

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
-$linuxPath = ".\monopoly\target\release\libmonopoly.so"
-$winPath = ".\monopoly\target\release\monopoly.dll"
+$linuxPath = "monopoly\target\release\libmonopoly.so"
+$winPath = "monopoly\target\release\monopoly.dll"
 
 if (!(Test-Path -Path $linuxPath) -or !(Test-Path -Path $winPath))
 {
@@ -9,6 +9,16 @@ if (!(Test-Path -Path $linuxPath) -or !(Test-Path -Path $winPath))
 
 .\clear.ps1
 mkdir build | Out-Null
+mkdir build\src | Out-Null
 
 cp index.py build
-# .cpython-312-x86_64-linux-gnu.so
+cp $linuxPath "build\monopoly.cpython-312-x86_64-linux-gnu.so"
+cp "src\__init__.py" "build\src"
+cp "src\lib.py" "build\src"
+cp "src\db.py" "build\src"
+cp requirements.txt build
+
+cp $winPath ".\monopoly.pyd"
+cp $linuxPath ".\monopoly.cpython-310-x86_64-linux-gnu.so"
+
+Compress-Archive -Path build\* -DestinationPath build.zip
--- clear.ps1
+++ clear.ps1
@@ -1,9 +1,9 @@
-$artifacts = "build.zip", "monopoly.pyd", "monopoly.*.so"
-
 if (Test-Path -Path build) 
 {
     Remove-Item -Recurse -Path build   
 }
+
+$artifacts = "build.zip", "monopoly.pyd", "monopoly.*.so"
 foreach ($artifact in $artifacts)
 {
     if (Test-Path -Path $artifact)
