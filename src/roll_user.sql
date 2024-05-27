6a1b3018e4d17a3578a091854309ec38af053f90
generic-name-2166 <kirbesson277@mail.ru>
2024-05-28 01:16:56 +0300

    fix(db): multiple queries fix

--- src/roll_user.sql
+++ src/roll_user.sql
@@ -1,8 +1,11 @@
-UPDATE chat SET "position" = %(position)s, money = %(money)s
-WHERE player_id = (
-    SELECT player_id FROM chat
-    WHERE user_id = %(user_id)s AND chat_id = %(chat_id)s
-);
+DO $$
+BEGIN
+    UPDATE chat SET "position" = %(position)s, money = %(money)s
+    WHERE player_id = (
+        SELECT player_id FROM chat
+        WHERE user_id = %(user_id)s AND chat_id = %(chat_id)s
+    );
 
-UPDATE game SET status = %(status)s
-WHERE chat_id = %(chat_id)s;
+    UPDATE game SET status = %(status)s
+    WHERE chat_id = %(chat_id)s;
+END $$;
--- src/start_user.sql
+++ src/start_user.sql
@@ -1,7 +1,10 @@
-INSERT INTO chat  (chat_id, user_id, "position", money) 
-VALUES (%(chat_id)s, %(user_id)s, %(position)s, %(money)s)
-ON CONFLICT DO NOTHING;
+DO $$
+BEGIN
+    INSERT INTO chat  (chat_id, user_id, "position", money) 
+    VALUES (%(chat_id)s, %(user_id)s, %(position)s, %(money)s)
+    ON CONFLICT DO NOTHING;
 
-INSERT INTO meta (user_id, username)
-VALUES (%(user_id)s, %(username))
-ON CONFLICT DO NOTHING;
+    INSERT INTO meta (user_id, username)
+    VALUES (%(user_id)s, %(username))
+    ON CONFLICT DO NOTHING;
+END $$;
