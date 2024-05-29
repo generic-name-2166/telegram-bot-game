c030d9a3f6e6c92d2eb66b2471d78a7f5793755a
generic-name-2166 <kirbesson277@mail.ru>
2024-05-29 21:58:30 +0300

    fix: wrong starting money amount

--- src/begin_user.sql
+++ src/begin_user.sql
@@ -1,4 +1,4 @@
-UPDATE chat SET "position" = 1, money = 2500
+UPDATE chat SET "position" = 1, money = 1500
 WHERE player_id = (
     SELECT player_id FROM chat
     WHERE user_id = %(user_id)s AND chat_id = %(chat_id)s
